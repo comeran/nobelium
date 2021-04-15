2f7acf31f81cbe2c1bff0b2c9e8f6f16c08df403
Homeran <ran826940919@163.com>
2021-04-15 16:29:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'Zane',
+  author: 'Zane',
+  email: 'zane@zane.net',
+  link: 'https://zane.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
   appearance: 'auto', // ['light', 'dark', 'auto'],
