8813714c74319870ff394bcbadf115f290997866
Homeran <ran826940919@163.com>
2021-04-15 16:31:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
   postsPerPage: 7,
   showAbout: true, // WIP
   showArchive: true, // WIP
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/homeran',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
